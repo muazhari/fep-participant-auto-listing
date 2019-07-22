adbcb9e2054222154e82bafbe7ed600366fda2a2
Muhammad Kharisma Azhari <39398937+muazhari@users.noreply.github.com>
2019-07-22 20:58:47 +0700

    Dev version

--- bothub/bot.py
+++ bothub/bot.py
@@ -76,8 +76,71 @@ class Bot(BaseBot):
         }
         """
 
-        # self.send_message('Echo: {}'.format(event['content']))
-        self.command(event)
+        if 'event_type' in event.keys():
+            if event['event_type'] != 'message':
+                self.send_message('Join event!')
+
+        self.command_handler(event)
+
+    def admin(self, args):
+        data = {
+            'event': args[-1]
+        }
+        event = data['event']
+        if self.whitelist_check(event):
+            self.send_message('privilage: {}'.format(event))
+
+    def blacklist_check(self, event):
+        sender = event['sender']
+
+        blocked_ids = ['C7b132b65f0db5c28c4b7563bd348d168',
+                       'C2a14eb4c73958925b6a299fe6798b67b']
+        blocked_types = []
+
+        validate = sender['id'] in blocked_ids or sender['name'] in blocked_types
+
+        return validate
+
+    def whitelist_check(self, event):
+        sender = event['sender']
+
+        allowed_ids = ['U016bfe22df53b903b404a80efdd8ec65', 'localuser']
+
+        allowed_types = []
+
+        validate_sender = sender['id'] in allowed_ids or sender['name'] in allowed_types
+
+        return validate_sender
+
+    def admin(self, args):
+        data = {
+            'event': args[-1]
+        }
+        event = data['event']
+        if self.whitelist_check(event):
+            self.send_message('privilage: {}'.format(event))
+
+    def blacklist_check(self, event):
+        sender = event['sender']
+
+        blocked_ids = ['C7b132b65f0db5c28c4b7563bd348d168',
+                       'C2a14eb4c73958925b6a299fe6798b67b']
+        blocked_types = []
+
+        validate = sender['id'] in blocked_ids or sender['name'] in blocked_types
+
+        return validate
+
+    def whitelist_check(self, event):
+        sender = event['sender']
+
+        allowed_ids = ['U016bfe22df53b903b404a80efdd8ec65', 'localuser']
+
+        allowed_types = []
+
+        validate_sender = sender['id'] in allowed_ids or sender['name'] in allowed_types
+
+        return validate_sender
 
     def add(self, args):
         if len(args) == 5:
@@ -86,12 +149,11 @@ class Bot(BaseBot):
                 'name': args[1],
                 'campus': args[2],
                 'room': args[3],
-                'user_id': args[-1]
+                'user_id': args[-1]['sender']['id']
             }
 
             self.set_store(data)
             self.view([args[0], args[-1]])
-            # self.send_message('Done!')
             self.backup_store('silent')
         else:
             self.send_message(Bot.command_prefix
@@ -105,7 +167,7 @@ class Bot(BaseBot):
                 'name': args[2],
                 'campus': args[3],
                 'room': args[4],
-                'user_id': args[-1]
+                'user_id': args[-1]['sender']['id']
             }
 
             self.update_store(data)
@@ -120,7 +182,7 @@ class Bot(BaseBot):
             data = {
                 'batch': args[0],
                 'num': args[1],
-                'user_id': args[-1]
+                'user_id': args[-1]['sender']['id']
             }
             self.delete_store(data)
             self.send_message('Done!')
@@ -133,25 +195,18 @@ class Bot(BaseBot):
         if len(args) <= 2:
             data = {
                 'batch': args[0],
-                'user_id': args[-1]
+                'user_id': args[-1]['sender']['id']
             }
             store = self.get_project_data('fep')
 
-            if len(args) == 1:
-                selected_batch = sorted(store.keys())
-            else:
-                selected_batch = [data['batch'].lower()]
+            if store is not None:
 
-            header = 'FEP BINUSIAN IT\n(Nama - Kampus - Nomor Ruangan)\n\n'
+                if len(args) == 1:
+                    selected_batch = sorted(store.keys())
+                else:
+                    selected_batch = [data['batch'].lower()]
 
-            if store != None:
-                # for batch in store.keys():
-                #     msg += '{}. {}\n'.format(batch.upper(), Bot.batch_list[batch])
-                #     for user, i in zip(store[batch].keys(), range(1, len(store[batch].keys()) + 1)):
-                #         msg += '{}. {} - {} - {}\n'.format(
-                #             i,  store[batch][user]['name'], store[batch][user]['campus'], store[batch][user]['room'])
-                #
-                #     msg += '\n'
+                header = 'FEP BINUSIAN IT\n(Nama - Kampus - Nomor Ruangan)\n\n'
 
                 for batch in selected_batch:
                     msg = header + \
@@ -171,19 +226,9 @@ class Bot(BaseBot):
 
             store = self.get_project_data('fep')
 
-            # if data['batch'] not in store.keys():
-            #     store[data['batch']] = {}
-            #
-            # selected_user_data = {key: data[key]
-            #                       for key in data.keys() if key not in ['batch', 'user_id']}
-            #
-            # store[data['batch']][data['user_id']] = selected_user_data
-
             if data['batch'] not in store.keys():
                 store[data['batch']] = []
 
-            # selected_user_data = {key: data[key]
-            #                       for key in data.keys() if key not in ['batch', 'user_id']}
             selected_user_data = [data['name'], data['campus'], data['room']]
 
             store[data['batch']].append(selected_user_data)
@@ -193,13 +238,6 @@ class Bot(BaseBot):
     def update_store(self, data):
         if data['batch'].lower() in Bot.batch_list.keys():
             store = self.get_project_data('fep')
-            # if data['batch'] not in store.keys():
-            #     store[data['batch']] = {}
-            #
-            # selected_user_data = {key: data[key]
-            #                       for key in data.keys() if key not in ['batch', 'user_id']}
-            #
-            # store[data['batch']][data['user_id']] = selected_user_data
 
             try:
                 selected_user_data = [data['name'],
@@ -215,11 +253,6 @@ class Bot(BaseBot):
 
     def delete_store(self, data):
         store = self.get_project_data('fep')
-        # for key in store.keys():
-        #     if data['user_id'] in store[key]:
-        #         store[key].pop(data['user_id'], None)
-        #         self.set_project_data(store)
-        # store[data['batch']].pop(int(data['num']) - 1)
 
         store[data['batch']].pop(int(data['num']) - 1)
         if len(store[data['batch']]) == 0:
@@ -235,11 +268,11 @@ class Bot(BaseBot):
 
     def pre_store(self, args):
         if len(args) <= 2:
-            default_url = 'https://gist.githubusercontent.com/muazhari/38a5819eb228a20a693db0516e76bedb/raw/108e665e24b63184f92444436b83142a4bf1fb0b/feplist'
+            default_url = 'https://gist.githubusercontent.com/muazhari/38a5819eb228a20a693db0516e76bedb/raw/5fe8b969ab5d3286f31026951edbb73ea030b460/feplist'
 
             data = {
                 'url': default_url if len(args) == 1 else args[0],
-                'user_id': args[-1]
+                'user_id': args[-1]['sender']['id']
             }
 
             feplc = cleaner(data['url'])
@@ -271,14 +304,25 @@ class Bot(BaseBot):
         backup_store['fep'].append({datetime.now(): response})
         self.set_project_data({'fep': store, 'backup': backup_store})
 
-    def command(self, event):
+    def help(self):
+        msg = '''
+        add <batch> <name> <campus> <room> - .add a kamu Kemanggisan 000
+        upd <batch> <number> <name> <campus> <room> - .upd a 1 kamu Kemanggisan 000
+        del <batch> <number> - .del a 1
+        view <batch> - .view a / .view
+        '''
+        self.send_message(msg)
+
+    def command_handler(self, event):
         command_list = {'add': self.add,
                         'upd': self.update,
                         'del': self.delete,
                         'view': self.view,
                         'reset_store': self.reset_store,
                         'pre_store': self.pre_store,
-                        'backup_store': self.backup_store
+                        'backup_store': self.backup_store,
+                        ']]': self.admin,
+                        'help': self.help,
                         }
 
         content_splitted = event['content'].split(' ')
@@ -286,13 +330,60 @@ class Bot(BaseBot):
         content_prefix = content_splitted[0][0]
         content_command = content_splitted[0][1:]
 
-        content_args = content_splitted[1:]
+        content_args = list(map(str.strip, content_splitted[1:]))
 
-        content_args.append(event['sender']['id'])
-
+        content_args.append(event)
         if content_prefix == Bot.command_prefix and content_command in command_list.keys():
             if len(getfullargspec(command_list[content_command]).args) > 1:
-                if '' not in content_args:
-                    command_list[content_command](content_args)
+                command_list[content_command](content_args)
             else:
                 command_list[content_command]()
+
+    @command('bimay')
+    def command_handlerbimay(self, event, context, args):
+        pass
+
+    @command('kurikulum')
+    def command_kurikulum(self, event, context, args):
+        pass
+
+    @command('fep')
+    def command_fep(self, event, context, args):
+        pass
+
+    @command('feprules')
+    def command_feprules(self, event, context, args):
+        pass
+
+    @command('go')
+    def command_go(self, event, context, args):
+        pass
+
+    @command('ao')
+    def command_ao(self, event, context, args):
+        pass
+
+    @command('clo')
+    def command_clo(self, event, context, args):
+        pass
+
+    @command('news')
+    def command_news(self, event, context, args):
+        pass
+
+    @command('parttime')
+    def command_parttime(self, event, context, args):
+        pass
+
+    @command('sat')
+    def command_sat(self, event, context, args):
+        pass
+
+    @command('himti')
+    def command_himti(self, event, context, args):
+        pass
+
+    @command(']]')
+    def command_admin(self, event, context, args):
+        self.send_message([event, context, args])
+        pass
--- bothub/feplist_cleaner.py
+++ bothub/feplist_cleaner.py
@@ -6,9 +6,10 @@ def hasNumbers(inputString):
 
 
 class cleaner:
-    def __init__(self, url):
+    def __init__(self, url='https://gist.githubusercontent.com/muazhari/38a5819eb228a20a693db0516e76bedb/raw/108e665e24b63184f92444436b83142a4bf1fb0b/feplist'):
         self.file = requests.get(url).text.splitlines()
         self.store = {}
+        self.batch_list = ['a', 'b', 'c', 'd', 'e']
 
     def set_store(self, data):
         if data['batch'] not in self.store.keys():
@@ -19,9 +20,25 @@ class cleaner:
 
         self.store[data['batch']].append(selected_user_data)
 
+    def numbers_sect(self, args):
+        num = ''
+        for i in args[0]:
+            if hasNumbers(i):
+                args[0] = args[0].replace(i, '').strip()
+                num += i
+
+            if args[0][0] == '.':
+                args[0] = args[0][1:].strip()
+
+        return num
+
+    def batch_validate(self, str):
+        return not hasNumbers(str) and str.lower() in self.batch_list
+
     def run(self):
+        intersected_line = []
         for line in range(0, len(self.file)):
-            if len(self.file[line]) > 0 and not hasNumbers(self.file[line][0]):
+            if len(self.file[line]) > 0 and self.batch_validate(self.file[line][0]):
                 batch = self.file[line][0].lower()
                 continue
 
@@ -30,17 +47,13 @@ class cleaner:
 
             args = list(map(str.strip, lsplit))
 
-            for i in args[0]:
-                if hasNumbers(i):
-                    args[0] = args[0].replace(i, '').strip()
+            args.insert(0, self.numbers_sect(args))
 
             if len(args) >= 1 and '' not in args:
-                if args[0][0] == '.':
-                    args[0] = args[0][1:].strip()
                 data = {
                     'batch': batch,
-                    'name': args[0],
-                    'campus': args[1],
-                    'room': '' if len(args) < 3 else args[-1],
+                    'name': args[1],
+                    'campus': args[2],
+                    'room': '' if len(args) < 4 else args[3],
                 }
                 self.set_store(data)
